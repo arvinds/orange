3d8c103897df3afdfd46e6df61937c8f47c5ecf7
Arvind Shrihari <arvindshrihari@outlook.com>
2013-05-25 05:51:04 +0000

    updating lost changes

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,9 @@
-document.write("test");
+var setupEditor = function(ace){
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/java");
+};
+
+
+require.config({ paths: {ace: "js/ace/ace/lib/ace"} });
+require(["ace/ace"], setupEditor);
